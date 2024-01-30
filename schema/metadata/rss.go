5672517f8028810fcfd80b6d7817b5d7592005d8
Henry <hi@henry.wang>
2024-01-30 19:53:32 +0800

    fix: correct RSS/feed omitempty

--- schema/metadata/rss.go
+++ schema/metadata/rss.go
@@ -7,14 +7,14 @@ import (
 var _ Metadata = (*RSS)(nil)
 
 type RSS struct {
-	Title       string    `json:"title,omitempty"`
-	Description string    `json:"description,omitempty"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
 	PubDate     string    `json:"pub_date,omitempty"`
 	Authors     []Authors `json:"authors,omitempty"`
 }
 
 type Authors struct {
-	Name string `json:"name,omitempty"`
+	Name string `json:"name"`
 }
 
 func (r RSS) Type() filter.Type {
