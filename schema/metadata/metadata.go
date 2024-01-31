f0c4c234502e5a582c6d9a50d38d4c133adf02e5
pseudoyu <pseudoyu@connect.hku.hk>
2024-01-31 11:35:58 +0800

    fix: missing metadata tags and types

--- schema/metadata/metadata.go
+++ schema/metadata/metadata.go
@@ -19,6 +19,10 @@ func Unmarshal(metadataType filter.Type, data json.RawMessage) (Metadata, error)
 		return unmarshalTransactionMetadata(metadataType, data)
 	case filter.TagSocial:
 		return unmarshalSocialMetadata(metadataType, data)
+	case filter.TagExchange:
+		return unmarshalExchangeMetadata(metadataType, data)
+	case filter.TagMetaverse:
+		return unmarshalMetaverseMetadata(metadataType, data)
 	case filter.TagRSS:
 		return unmarshalRSSMetadata(metadataType, data)
 	default:
@@ -32,6 +36,10 @@ func unmarshalCollectibleMetadata(metadataType filter.Type, data json.RawMessage
 	switch metadataType {
 	case filter.TypeCollectibleTransfer, filter.TypeCollectibleMint, filter.TypeCollectibleBurn:
 		result = new(CollectibleTransfer)
+	case filter.TypeCollectibleApproval:
+		result = new(CollectibleApproval)
+	case filter.TypeCollectibleTrade:
+		result = new(CollectibleTrade)
 	default:
 		return nil, fmt.Errorf("invalid metadata type: %s.%s", metadataType.Tag(), metadataType.Name())
 	}
@@ -49,8 +57,10 @@ func unmarshalTransactionMetadata(metadataType filter.Type, data json.RawMessage
 	switch metadataType {
 	case filter.TypeTransactionApproval:
 		result = new(TransactionApproval)
-	case filter.TypeTransactionTransfer:
+	case filter.TypeTransactionTransfer, filter.TypeTransactionMint, filter.TypeTransactionBurn:
 		result = new(TransactionTransfer)
+	case filter.TypeTransactionBridge:
+		result = new(TransactionBridge)
 	default:
 		return nil, fmt.Errorf("invalid metadata type: %s.%s", metadataType.Tag(), metadataType.Name())
 	}
@@ -66,8 +76,52 @@ func unmarshalSocialMetadata(metadataType filter.Type, data json.RawMessage) (Me
 	var result Metadata
 
 	switch metadataType {
-	case filter.TypeSocialPost, filter.TypeSocialComment, filter.TypeSocialShare:
+	case filter.TypeSocialPost, filter.TypeSocialRevise, filter.TypeSocialComment, filter.TypeSocialDelete, filter.TypeSocialShare, filter.TypeSocialReward, filter.TypeSocialMint:
 		result = new(SocialPost)
+	case filter.TypeSocialProfile:
+		result = new(SocialProfile)
+	case filter.TypeSocialProxy:
+		result = new(SocialProxy)
+	default:
+		return nil, fmt.Errorf("invalid metadata type: %s.%s", metadataType.Tag(), metadataType.Name())
+	}
+
+	if err := json.Unmarshal(data, &result); err != nil {
+		return nil, fmt.Errorf("invalid metadata: %w", err)
+	}
+
+	return result, nil
+}
+
+func unmarshalExchangeMetadata(metadataType filter.Type, data json.RawMessage) (Metadata, error) {
+	var result Metadata
+
+	switch metadataType {
+	case filter.TypeExchangeSwap:
+		result = new(ExchangeSwap)
+	case filter.TypeExchangeLiquidity:
+		result = new(ExchangeLiquidity)
+	case filter.TypeExchangeStaking:
+		result = new(ExchangeStaking)
+	default:
+		return nil, fmt.Errorf("invalid metadata type: %s.%s", metadataType.Tag(), metadataType.Name())
+	}
+
+	if err := json.Unmarshal(data, &result); err != nil {
+		return nil, fmt.Errorf("invalid metadata: %w", err)
+	}
+
+	return result, nil
+}
+
+func unmarshalMetaverseMetadata(metadataType filter.Type, data json.RawMessage) (Metadata, error) {
+	var result Metadata
+
+	switch metadataType {
+	case filter.TypeMetaverseMint, filter.TypeMetaverseBurn, filter.TypeMetaverseTransfer:
+		result = new(MetaverseTransfer)
+	case filter.TypeMetaverseTrade:
+		result = new(MetaverseTrade)
 	default:
 		return nil, fmt.Errorf("invalid metadata type: %s.%s", metadataType.Tag(), metadataType.Name())
 	}
