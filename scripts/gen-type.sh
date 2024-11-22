2f53ca2eb5b388233bbae8b399e2f1facf587b96
incubator4 <aries0robin@gmail.com>
2024-11-22 13:48:21 +0800

    fix(gen-type.sh): replace oneOf to allOf

--- scripts/gen-type.sh
+++ scripts/gen-type.sh
@@ -7,7 +7,7 @@ if [ ! -f "$file" ]; then
   touch "$file"
 fi
 echo "type: string" > "$file"
-echo "oneOf:" >> "$file"
+echo "allOf:" >> "$file"
 
 find ./openapi/type -name '*.yaml' | cut -d '/' -f 4 | while read -r type; do
   echo "  - \$ref: \"./type/$type\"" >> "openapi/Type.yaml"
